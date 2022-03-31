672a90b8cebbb256661b9917d044fafbf833b447
ccandy <ccunandy2001@gmail.com>
2022-03-31 13:09:27 -0700

    add smith ggx fast

--- Assets/ShaderLibrary/Lighting/FilamentPBR/FilamentLighting.hlsl
+++ Assets/ShaderLibrary/Lighting/FilamentPBR/FilamentLighting.hlsl
@@ -23,4 +23,43 @@ float DGGX(FilamentSurface surface, PBRLight light, float3 viewDir)
 	return ggx;
 }
 
+float SmithGGX(FilamentSurface surface, PBRLight light, float3 viewDir)
+{
+	float roughness = surface.Roughness;
+	float a2 = Pow2(roughness);
+
+	float3 normal = surface.Normal;
+	float3 lightDir = light.LightDirection;
+	float NdotL = Dot(normal, lightDir);
+	float NdotV = Dot(normal, viewDir);
+
+	float NdotL2 = Pow2(NdotL);
+	float NdotV2 = Pow2(NdotV);
+
+	float GGXV = NdotL * sqrt(NdotV2 * (1 - a2) + a2);
+	float GGXL = NdotV * sqrt(NdotL2 * (1 - a2) + a2);
+
+	float sggx = 0.5 / (GGXV + GGXL);
+
+	return sggx;
+}
+
+float SmithGGXFast(FilamentSurface surface, PBRLight light, float3 viewDir) 
+{
+	float a = surface.Roughness;
+
+	float3 normal = surface.Normal;
+	float3 lightDir = light.LightDirection;
+	float NdotL = Dot(normal, lightDir);
+	float NdotV = Dot(normal, viewDir);
+
+	float GGXV = NdotL * sqrt(NdotV * (1 - a) + a);
+	float GGXL = NdotV * sqrt(NdotL * (1 - a) + a);
+
+	float sggx = 0.5 / (GGXV + GGXL);
+
+	return sggx;
+	
+}
+
 #endif
